7afb5eb9d252447e978fa777aaad752d73670c15
owenliversidge <owen.liversidge@gmail.com>
2019-02-21 11:36:58 -0500

    added basic state plus next and previous handlers to Wizzard

--- src/core/components/wizard/Wizard.js
+++ src/core/components/wizard/Wizard.js
@@ -1,6 +1,37 @@
 import React, { Component } from "react";
 
 export default class Wizard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      wizardContext: this.props.wizardContext,
+      currentStep: 1
+    };
+
+    this.nextStep = this.nextStep.bind(this);
+    this.previousStep = this.previousStep.bind(this);
+  }
+
+  submit(finalLabel) {
+    this.props.handleSubmit(finalLabel);
+  }
+
+  nextStep() {
+    this.setState(prevState => {
+      return {
+        currentStep: prevState.currentStep + 1
+      };
+    });
+  }
+
+  previousStep() {
+    if (this.state.currentStep > 1) {
+      this.setState({
+        currentStep: this.state.currentStep - 1
+      });
+    }
+  }
+
   render() {
     return <div />;
   }
