b5f8062bb7e52a0e4c4dac9c4ee225584d246cf7
owenliversidge <owen.liversidge@gmail.com>
2019-02-21 12:03:13 -0500

    added label schema

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
+import Wizard from "./core/components/wizard/Wizard";
+import LabelSchema from "./static-components/LabelSchema";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Test Page</h1>
+        <Wizard wizardContext={LabelSchema} />
       </div>
     );
   }
