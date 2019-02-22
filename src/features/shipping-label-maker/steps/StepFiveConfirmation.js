71bfb621848961a1e9578d2b2ea5c4118631a21e
owenliversidge <owen.liversidge@gmail.com>
2019-02-22 14:46:27 -0500

    confirmation page starting to happen

--- src/features/shipping-label-maker/steps/StepFiveConfirmation.js
+++ src/features/shipping-label-maker/steps/StepFiveConfirmation.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
+import { Container, Segment } from "semantic-ui-react";
 
 export default class StepFiveConfirmation extends Component {
   constructor(props) {
     super(props);
     this.state = {
       wizardContext: this.props.wizardContext,
-      shippingCost: 0
+      shippingTotal: 0
     };
 
     // this.confirm = this.confirm.bind(this);
@@ -29,11 +30,69 @@ export default class StepFiveConfirmation extends Component {
         shippingRate *
         (shippingOption === shippingOption.ground ? 1 : 1.5);
 
-    const finalCost = shippingCost.toFixed(2);
-    this.setState({ shippingCost: finalCost });
+    const shippingTotal = shippingCost.toFixed(2);
+    this.setState({ shippingTotal });
   };
 
+  confirm() {
+    this.props.onAction(this.state);
+  }
+
   render() {
-    return <div>Dummy text</div>;
+    const { wizardContext } = this.state;
+    return (
+      <Container
+        style={{
+          border: "solid",
+          borderWidth: "0.5px",
+          borderColor: "grey",
+          borderRadius: "5px",
+          padding: "15px"
+        }}
+      >
+        <Segment.Group>
+          <Segment>From:</Segment>
+          <Segment.Group>
+            <Segment>{wizardContext.from.name}</Segment>
+            <Segment>{wizardContext.from.street}</Segment>
+            <Segment.Group horizontal>
+              <Segment>{wizardContext.from.city}</Segment>
+              <Segment>{wizardContext.from.state}</Segment>
+              <Segment>{wizardContext.from.zip}</Segment>
+            </Segment.Group>
+          </Segment.Group>
+        </Segment.Group>
+        <Segment.Group>
+          <Segment>To:</Segment>
+          <Segment.Group>
+            <Segment>{wizardContext.to.name}</Segment>
+            <Segment>{wizardContext.to.street}</Segment>
+            <Segment.Group horizontal>
+              <Segment>{wizardContext.to.city}</Segment>
+              <Segment>{wizardContext.to.state}</Segment>
+              <Segment>{wizardContext.to.zip}</Segment>
+            </Segment.Group>
+          </Segment.Group>
+        </Segment.Group>
+        <Segment.Group>
+          <Segment>Weight:</Segment>
+          <Segment.Group>
+            <Segment>{wizardContext.weight} lbs</Segment>
+          </Segment.Group>
+        </Segment.Group>
+        <Segment.Group>
+          <Segment>Shipping Method:</Segment>
+          <Segment.Group>
+            <Segment>{wizardContext.shippingOption}</Segment>
+          </Segment.Group>
+        </Segment.Group>
+        <Segment.Group style={{ fontWeight: "bold" }}>
+          <Segment>Shipping Total:</Segment>
+          <Segment.Group>
+            <Segment>${this.state.shippingTotal}</Segment>
+          </Segment.Group>
+        </Segment.Group>
+      </Container>
+    );
   }
 }
