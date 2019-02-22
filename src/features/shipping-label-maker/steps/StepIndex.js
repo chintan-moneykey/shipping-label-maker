14bcbad26f0bc3cfc792c7161aeb21870dbdbd25
owenliversidge <owen.liversidge@gmail.com>
2019-02-22 09:39:26 -0500

    added shipping step

--- src/features/shipping-label-maker/steps/StepIndex.js
+++ src/features/shipping-label-maker/steps/StepIndex.js
@@ -2,6 +2,7 @@ import React from "react";
 import StepOneSenderDetails from "./StepOneSenderDetails";
 import StepTwoRecipientDetails from "./StepTwoRecipientDetails";
 import StepThreeWeight from "./StepThreeWeight";
+import StepFourShipping from "./StepFourShipping";
 
 const steps = [
   <StepOneSenderDetails
@@ -12,7 +13,11 @@ const steps = [
     wizardContext={"to"}
     onAction={"handleNameAndAddress"}
   />,
-  <StepThreeWeight wizardContext={"weight"} onAction={"handleRootState"} />
+  <StepThreeWeight wizardContext={"weight"} onAction={"handleRootState"} />,
+  <StepFourShipping
+    wizardContext={"shippingOption"}
+    onAction={"handleRootState"}
+  />
 ];
 
 export default steps;
