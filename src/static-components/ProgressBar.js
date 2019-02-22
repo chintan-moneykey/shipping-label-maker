2f5372661a38019b696fb136e6ccd461a121a7d5
owenliversidge <owen.liversidge@gmail.com>
2019-02-22 12:25:56 -0500

    okay, had to correct that last one

--- src/static-components/ProgressBar.js
+++ src/static-components/ProgressBar.js
@@ -3,13 +3,13 @@ import { Progress } from "semantic-ui-react";
 
 export class ProgressBar extends Component {
   render() {
-    const { progress } = this.props.currentStep;
+    const progress = this.props.currentStep;
 
     return (
       <div>
         <Progress
           color={progress > 4 ? "green" : "yellow"}
-          value={this.props.currentStep}
+          value={progress}
           total="5"
           progress="ratio"
         />
