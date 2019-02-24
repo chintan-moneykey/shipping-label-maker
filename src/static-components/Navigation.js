fe9fa55ae2677c3988a0793efac44a7c105da0b5
owenliversidge <owen.liversidge@gmail.com>
2019-02-24 09:29:56 -0500

    buttons now in required position

--- src/static-components/Navigation.js
+++ src/static-components/Navigation.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Container, Button, Icon } from "semantic-ui-react";
+import styled from "styled-components";
 
 const Navigation = props => (
   <Container
@@ -7,28 +8,24 @@ const Navigation = props => (
       marginTop: "20px"
     }}
   >
-    {props.currentStep < 2 ? null : (
-      <Button
-        icon
-        labelPosition="left"
-        floated="left"
-        onClick={props.previousStep}
-      >
-        <Icon name="left arrow" />
-        Go Back
+    <ButtonContainer>
+      {props.currentStep < 2 ? null : (
+        <Button icon labelPosition="left" onClick={props.previousStep}>
+          <Icon name="left arrow" />
+          Go Back
+        </Button>
+      )}
+      <Button primary icon labelPosition="right" onClick={props.nextStep}>
+        Next
+        <Icon name="right arrow" />
       </Button>
-    )}
-    <Button
-      primary
-      icon
-      labelPosition="right"
-      floated="right"
-      onClick={props.nextStep}
-    >
-      Next
-      <Icon name="right arrow" />
-    </Button>
+    </ButtonContainer>
   </Container>
 );
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
 
 export default Navigation;
